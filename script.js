7ada8ab59cb4f23f29fc009fd0b0e8d7078f3f28
Andrii <andryshonochek0604@gmail.com>
2020-02-13 22:51:51 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+(function() {
+  let users = [
+    'iseldrew',
+    'oksanakozhukh',
+    'antonpanchenk0',
+    'giruta',
+    'vitaliyhtml5',
+    'staspitsyk',
+    'alexandrkrivobok',
+    'alekseypetrenko',
+    'kolesnicknick',
+  ];
+
+  function getRandomUser() {
+    if(!users.length) {
+      alert('No users left')
+      return
+    }
+
+    const randomNumber = Math.floor(Math.random()*users.length);
+    const user = users[randomNumber];
+    users = users.filter(item => { return item != user })
+
+    loadUser(user);
+  }
+
+  function loadUser(pickedUser) {
+    const link = 'https://api.github.com/users/' + pickedUser;
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', link, true);
+
+    xhr.onload = function() {
+      if(this.status === 200) {
+        const user = JSON.parse(this.responseText);
+
+        addUserToDOM(user);
+      }
+    }
+    xhr.send();
+  }
+
+  function addUserToDOM(user) {
+    const output =
+      '<div class="user">' +
+        '<img src="' + user.avatar_url + '" width="70" height="70">' +
+        '<br>' + 
+        '<a href="' + user.html_url +  '">' + `${user.login}` + '</a>' +
+      '</div>';
+
+    document.querySelector('.users').innerHTML = output;
+  }
+
+  document.querySelector('.hero').addEventListener('click', getRandomUser);
+})()
+
