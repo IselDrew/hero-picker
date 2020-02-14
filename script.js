ad8080dc22c6192e6681d5f74f76a6e0d53733ee
Andrii <andryshonochek0604@gmail.com>
2020-02-14 12:20:27 +0200

    Add async\await request

--- script.js
+++ script.js
@@ -1,56 +1,49 @@
-(function() {
-  let users = [
-    'iseldrew',
-    'oksanakozhukh',
-    'antonpanchenk0',
-    'giruta',
-    'vitaliyhtml5',
-    'staspitsyk',
-    'alexandrkrivobok',
-    'alekseypetrenko',
-    'kolesnicknick',
-  ];
+let users = [
+  'iseldrew',
+  'oksanakozhukh',
+  'antonpanchenk0',
+  'giruta',
+  'vitaliyhtml5',
+  'staspitsyk',
+  'alexandrkrivobok',
+  'alekseypetrenko',
+  'kolesnicknick',
+];
 
-  function getRandomUser() {
-    if(!users.length) {
-      alert('No users left')
-      return
-    }
-
-    const randomNumber = Math.floor(Math.random()*users.length);
-    const user = users[randomNumber];
-    users = users.filter(item => { return item != user })
-
-    loadUser(user);
+function getRandomUser() {
+  if(!users.length) {
+    alert('No users left')
+    return
   }
 
-  function loadUser(pickedUser) {
+  const randomNumber = Math.floor(Math.random()*users.length);
+  const user = users[randomNumber];
+  users = users.filter(item => { return item != user })
+
+  loadUser(user);
+}
+
+async function loadUser(pickedUser) {
+  try {
     const link = 'https://api.github.com/users/' + pickedUser;
-    let xhr = new XMLHttpRequest();
-
-    xhr.open('GET', link, true);
-
-    xhr.onload = function() {
-      if(this.status === 200) {
-        const user = JSON.parse(this.responseText);
-
-        addUserToDOM(user);
-      }
-    }
-    xhr.send();
+    let response = await fetch(link);
+    let user = await response.json();
+    addUserToDOM(user);
+  } catch (err) {
+    console.log(err);
   }
+}
 
-  function addUserToDOM(user) {
-    const output =
-      '<div class="user">' +
-        '<img src="' + user.avatar_url + '" >' +
-        '<br>' + 
-        '<a href="' + user.html_url +  '">' + `${user.login}` + '</a>' +
-      '</div>';
+function addUserToDOM(user) {
+  const output =
+    '<div class="user">' +
+      '<img src="' + user.avatar_url + '" >' +
+      '<br>' + 
+      '<a href="' + user.html_url +  '">' + `${user.login}` + '</a>' +
+    '</div>';
 
-    document.querySelector('.users').innerHTML = output;
-  }
+  document.querySelector('.users').innerHTML = output;
+}
 
-  document.querySelector('.hero').addEventListener('click', getRandomUser);
-})()
+document.querySelector('.hero').addEventListener('click', getRandomUser);
 
