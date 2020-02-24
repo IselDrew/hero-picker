d83541542c9befbd91213990c482b76bcaa5ddb3
IselDrew <andryshonochek0604@gmail.com>
2020-02-24 09:51:45 +0200

    Add handleClick

--- script.js
+++ script.js
@@ -3,80 +3,85 @@
 //    E.g. [{ name: 'IselDrew', isChecked: false }, {...}, ...]
 // 2. Add list of users with line-through for those, who has isChecked: true;
 // 3. Redo Reload without refreshing page;
-// 4. Rewrite Reload button;
+// 4. Rewrite Reload button; || done!
+(function(){
+  let users = [
+    'iseldrew',
+    'oksanakozhukh',
+    'antonpanchenk0',
+    'giruta',
+    'staspitsyk',
+    'alexandrkrivobok',
+    'alekseypetrenko',
+    'maslovakat',
+    // 'kolesnicknick',
+  ];
 
-let users = [
-  'iseldrew',
-  'oksanakozhukh',
-  'antonpanchenk0',
-  'giruta',
-  'staspitsyk',
-  'alexandrkrivobok',
-  'alekseypetrenko',
-  'maslovakat',
-  // 'kolesnicknick',
-];
+  const chooseButton = document.querySelector('.choose-button');
+  const usersInfo = {};
 
-const chooseButton = document.querySelector('.choose-button');
-
-const usersInfo = {};
-
-function getAllUsers() {
-  users.forEach(user => loadUser(user));
-  console.log(usersInfo);
-}
-
-async function loadUser(pickedUser) {
-  try {
-    const link = 'https://api.github.com/users/' + pickedUser;
-    let response = await fetch(link);
-    let user = await response.json();
-    usersInfo[pickedUser] = user;
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-function getRandomUser() {
-  if(!users.length) {
-    chooseButton.textContent = 'Reload' // delete this 
-    chooseButton.addEventListener('click', reloadPage) // delete this
-    showMessage('No Users Left')
-    return;
+  function getAllUsers() {
+    users.forEach(user => loadUser(user));
+    console.log(usersInfo);
   }
 
-  const randomNumber = Math.floor(Math.random()*users.length);
-  const gitAcc = users[randomNumber];
-  const user = usersInfo[gitAcc];
-  users = users.filter(item => { return item != gitAcc});
+  async function loadUser(pickedUser) {
+    try {
+      const link = 'https://api.github.com/users/' + pickedUser;
+      let response = await fetch(link);
+      let user = await response.json();
+      usersInfo[pickedUser] = user;
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-  addUserToDOM(user);
-}
+  function handleClick() {
+    if (chooseButton.textContent === 'Reload') {
+      reloadPage();
+    }
+    if(!users.length) {
+      chooseButton.textContent = 'Reload'
+      showMessage('No Users Left')
+    } else {
+      getRandomUser();
+    }
+  }
 
-function reloadPage() { // delete this
-  location.reload();
-}
+  function getRandomUser() {
+    const randomNumber = Math.floor(Math.random()*users.length);
+    const gitAcc = users[randomNumber];
+    const user = usersInfo[gitAcc];
+    users = users.filter(item => { return item != gitAcc});
 
-function showMessage(msg) {
-  const output = '<h1>' + msg + '</h1>';
-  document.querySelector('.users').innerHTML = output;
-}
+    addUserToDOM(user);
+  }
 
-function addUserToDOM(user) {
-  const output =
-    '<div class="user">' +
-      '<a href="' + user.html_url +  '" target="_blank">' + 
-        '<div class="img-wrapper">' +
-          '<img src="' + user.avatar_url + '" >' +
-        '</div>' +
-      '</a>' +
-      '<br>' + 
-      '<h1>' + user.login + '</h1>' +
-    '</div>';
+  function reloadPage() { // rewrite this function
+    location.reload();
+  }
 
-  document.querySelector('.users').innerHTML = output;
-}
 
-chooseButton.addEventListener('click', getRandomUser);
-document.addEventListener('DOMContentLoaded', getAllUsers)
+  function addUserToDOM(user) {
+    const output =
+      '<div class="user">' +
+        '<a href="' + user.html_url +  '" target="_blank">' + 
+          '<div class="img-wrapper">' +
+            '<img src="' + user.avatar_url + '" >' +
+          '</div>' +
+        '</a>' +
+        '<br>' + 
+        '<h1>' + user.login + '</h1>' +
+      '</div>';
 
+    document.querySelector('.users').innerHTML = output;
+  }
+
+  function showMessage(msg) {
+    const output = '<h1>' + msg + '</h1>';
+    document.querySelector('.users').innerHTML = output;
+  }
+
+  chooseButton.addEventListener('click', handleClick);
+  document.addEventListener('DOMContentLoaded', getAllUsers)
+})()
